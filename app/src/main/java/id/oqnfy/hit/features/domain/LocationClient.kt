bae48ee5433974cf515582644111610886a213dd
Oqinify <khoirul.stark@gmail.com>
2023-01-18 15:14:36 +0700

    Track user location in background

---
+++ app/src/main/java/id/oqnfy/hit/features/domain/LocationClient.kt
@@ -0,0 +1,10 @@
+package id.oqnfy.hit.features.domain
+
+import android.location.Location
+import kotlinx.coroutines.flow.Flow
+
+interface LocationClient {
+    fun getLocationUpdates(interval: Long): Flow<Location>
+
+    class LocationException(message: String): Exception()
+}
